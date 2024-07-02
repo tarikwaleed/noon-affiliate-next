d3e7c279ba79110a5c5c571803f0ed461f949e46
tarikwaleed <tarikwaleed.tech@gmail.com>
2024-07-02 11:43:53 +0300

    :lipstick:

--- src/components/coupon-card.tsx
+++ src/components/coupon-card.tsx
@@ -80,8 +80,16 @@ export function CouponCard({ couponCode }: { couponCode: string | undefined }) {
           <>
             <div className="flex flex-col items-center justify-center bg-green-300 p-6 rounded-lg">
               <h2 className="text-2xl font-bold mb-2">عمولاتك</h2>
-              <p className="text-8xl font-bold text-green-700">{couponDetails?.total_commission} <span className="text-4xl text-yellow-600">SAR</span></p>
-              <Link href={`https://wa.me/966543486623?text=%D8%A3%D8%B1%D9%8A%D8%AF%20%D8%B3%D8%AD%D8%A8%20%D8%B9%D9%85%D9%88%D9%84%D8%A7%D8%AA%D9%8A%20%D9%83%D9%88%D8%AF%20%D8%A7%D9%84%D8%AE%D8%B5%D9%85%20%D8%A7%D9%84%D9%85%D9%85%D9%8A%D8%B2%20%D8%A7%D9%84%D8%AE%D8%A7%D8%B5%20%D8%A8%D9%8A%20%D9%87%D9%88%20${couponCode}`}>
+              <p
+              className="text-8xl font-bold text-green-700">
+                        {couponDetails && new Intl.NumberFormat('en-US', { style: 'decimal', minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(couponDetails.total_commission)}
+            <span className="text-4xl text-yellow-600">ر. س</span>
+              </p>
+
+
+              <Link
+              target="_blank"
+              href={`https://wa.me/966543486623?text=%D8%A3%D8%B1%D9%8A%D8%AF%20%D8%B3%D8%AD%D8%A8%20%D8%B9%D9%85%D9%88%D9%84%D8%A7%D8%AA%D9%8A%20%D9%83%D9%88%D8%AF%20%D8%A7%D9%84%D8%AE%D8%B5%D9%85%20%D8%A7%D9%84%D9%85%D9%85%D9%8A%D8%B2%20%D8%A7%D9%84%D8%AE%D8%A7%D8%B5%20%D8%A8%D9%8A%20%D9%87%D9%88%20${couponCode}`}>
                 <Button className="mt-4">
                   <WalletIcon className="w-4 h-4 mr-2" />
                   اسحب عمولاتك
@@ -169,7 +177,7 @@ export function CouponCard({ couponCode }: { couponCode: string | undefined }) {
           :
           <>
             <div className="text-7xl">
-              Your Coupon didn't get any orders Yet
+            كوبونك لم يحصل على اي طلبات بعد
             </div>
           </>}
 
